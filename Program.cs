aabe9d77f199dfede98259d937d60a263fbd5f1b
colin-sonarsource <colin.mueller@sonarsource.com>
2023-08-21 11:00:29 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System.Text;
+using System.Linq;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var a = new KoeieHol();
+
+public class KoeieHol{
+    public KoeieHol(){}
+    public KoeieHol(string a, string b, string c, string d, string e, string f, string g, string h){
+        var aa = b;
+        var bb = a;
+    }
+}
